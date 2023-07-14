471f2004ed86b20d7f24006e8905d5a2506f13c9
Elen3242 <elenh9849@gmail.com>
2023-07-14 18:00:13 +0400

    add task list, write css code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './app.css';
 function App() {
 
     //tasks 
-    const [toDO , setToDo] = useState ([
+    const [toDo , setToDo] = useState ([
         {"id": 1, "title": 'Task 1' , "status": false},
         {"id": 2, "title": 'Task 2' , "status": false}
     ]);
@@ -32,7 +32,7 @@ function App() {
     }
 
     //cancelUpdate
-    const cancelUpdate = (id) => {
+    const cancelUpdate = () => {
 
     }
 
@@ -41,14 +41,35 @@ function App() {
 
     }
 
-    const updateTask = (e) => {
+    const updateTask = () => {
 
     }
 
         return (
             <div className="container App">
             <br></br>
-            <h2>To Do List App</h2>
+             <h2>To Do List App</h2>
+            <br></br>
+            {/* display toDo */}
+            {/* this code run when tasks absence  */}
+            {toDo && toDo.length ? '' : "No tasks..."} 
+
+            {toDo && toDo
+                .map (( task , index) => {
+                    return (
+                        <React.Fragment key={task.id}>
+                            <div className="col taskBg"> 
+                                <div className={task.status ? 'done' : ''}>
+                                    <span className="taskText">{index + 1} </span>
+                                    <span className="taskText">{task.title} </span>
+                                </div>
+                            </div>
+                             
+                            
+                        </React.Fragment>
+                    )
+                })
+            }
             </div>
         )
 }
