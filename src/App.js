ec51c66733d8c10c90115e8a300d5b6b70eb388f
Elen3242 <elenh9849@gmail.com>
2023-07-14 22:42:53 +0400

    add delete and adding fucntional

--- src/App.js
+++ src/App.js
@@ -13,16 +13,22 @@ function App() {
 
     //temp state 
     const [newTask , setNewTask ] = useState('');
-    const [updateData , setUpdateData] = useState('')
+    const [updateData , setUpdateData] = useState('');
 
     //add task
     const addTask = () => {
-
+        if(newTask) {
+            let num = toDo.length + 1;
+            let newEntry = {id: num , title: newTask, status: false}
+            setToDo([...toDo, newEntry])
+            setNewTask('')
+        }
     }
 
     //delete task
     const deleteTask = (id) => {
-
+        let newTasks = toDo.filter(task => task.id !== id)
+        setToDo(newTask)
     }
 
     //markDone
@@ -49,8 +55,47 @@ function App() {
             <br></br>
              <h2>To Do List App</h2>
             <br></br>
+            {/* Update Task */}
+            <div className="row">
+                <div className="col">
+                    <input 
+                    className="form-control form-control-lg"
+                    />
+                </div>
+
+                <div className="col-auto">
+                    <button className="btn btn-lg btn-success mr-20" >
+                        Update 
+                    </button>
+                    <button className="btn btn-lg btn-warning">
+                        Cancel
+                    </button>
+                </div>
+            </div>
+            <br></br>
+
+            {/* Add task */}
+            <div className="row">
+                <div className="col">
+                    <input 
+                    value={newTask}
+                    onChange={(e) => setNewTask(e.target.value)}
+                    className="form-control form-control-lg"
+                    />
+                </div>
+
+                <div className="col-auto">
+                <button 
+                    className="btn btn-lg btn-success"
+                    onClick={addTask}
+                >
+                    Add Task 
+                </button>
+                </div>
+            </div>
+            <br></br>
             {/* display toDo */}
-            {/* this code run when tasks absence  */}
+            {/* this code run when tasks absence  (line 10)*/}
             {toDo && toDo.length ? '' : "No tasks..."} 
 
             {toDo && toDo 
@@ -71,7 +116,9 @@ function App() {
                                     <span title="Edit">
                                         <FontAwesomeIcon icon={faPen} />
                                     </span>
-                                    <span title="Delete">
+                                    <span title="Delete"
+                                        onClick={() => deleteTask(task.id)}
+                                    >
                                         <FontAwesomeIcon icon={faTrashCan} />
                                     </span>
                                 </div>
